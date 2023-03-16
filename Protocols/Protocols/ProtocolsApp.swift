8901a545c4922e1f2fdae286710356a38ef1ce2b
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-16 13:34:08 -0700

    introduced the protocol

---
+++ Protocols/Protocols/ProtocolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtocolsApp.swift
+//  Protocols
+//
+//  Created by Jonathan Bigbee on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ProtocolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
