6088bde748612517cf68efaf4a713e590f03ab8b
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-17 12:59:51 -0700

    adopted protocols

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -10,3 +10,27 @@ protocol Taggable {
     
     init(tag: String, data: Data)
 }
+
+struct TaggedData: Taggable {
+    var tag: String
+    
+    var data: Data
+    
+    static var counter: Int = 0
+    
+    mutating func update(data: Data) -> Bool {
+        self.data = data
+        return true
+    }
+    
+    static func icrementCounter() {
+        counter += 1
+    }
+    
+    init(tag: String, data: Data) {
+        self.tag = tag
+        self.data = data
+    }
+    
+    
+}
