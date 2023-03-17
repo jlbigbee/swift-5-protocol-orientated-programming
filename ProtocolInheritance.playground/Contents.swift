226e6250785c01fde2ebce0c5024ced820d45b7a
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-17 14:06:07 -0700

    adopted multiple protocols

--- ProtocolInheritance.playground/Contents.swift
+++ ProtocolInheritance.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 protocol Taggable {
-    var tag: String {get}
+    var tag: String { get }
     var data: Data { get }
     init(tag: String, data: Data)
 }
@@ -13,4 +13,38 @@ protocol TaggedPersistable: Taggable, CustomStringConvertible, Equatable {
 
 protocol TaggedEncodable: Taggable {
     var base64: String { get }
+}
+
+struct MyData: Taggable {
+    
+    var tag: String
+    var data: Data
+    
+    init(tag: String, data: Data) {
+        self.tag = tag
+        self.data = data
+    }
+}
+
+extension MyData: CustomStringConvertible {
+    var description: String {
+        return "MyData(\(tag)"
+    }
+}
+
+extension MyData: TaggedEncodable {
+    var base64: String {
+        return self.data.base64EncodedString()
+    }
+}
+
+extension MyData: TaggedPersistable {
+    init(tag: String, contentOf url: URL) throws {
+        let data = try Data.init(contentsOf: url)
+        self.init(tag: tag, data: data)
+    }
+    
+    func persist(to url: URL) throws {
+        try self.data.write(to: url)
+    }
 }
