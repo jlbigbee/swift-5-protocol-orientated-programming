60616cfbfaee2918ea442b970db08b786bdf00eb
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-22 20:43:43 -0700

    used generics with protocols

--- TaggedData.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ TaggedData.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 protocol Taggable {
-    associatedtype Content
+    associatedtype Content: Equatable
     
     var tag: String { get }
     var data: Content { get }
@@ -18,3 +18,11 @@ struct TaggedDate: Taggable {
     var tag: String
     var data: Date
 }
+
+struct GenericTagged <T: Equatable>: Taggable {
+    var tag: String
+    var data: T
+}
+
+let taggedDouble = GenericTagged(tag: "pi", data: Double.pi) //GenericTagged<Double>(tag: "pi", data: Double.pi)
+
